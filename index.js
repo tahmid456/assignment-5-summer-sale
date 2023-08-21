5151830dab63f89e7af0fcc499752ab2b7811ab5
unknown <imrojtahmid@gmail.com>
2023-08-21 13:01:18 +0600

    updated

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ p.innerText = "K.accessories"
 itemsAll.appendChild(p);
 
 
+const firstKitchenPrice = document.getElementById("first-kitchen");
+const firstKitchenPriceString = firstKitchenPrice.innerText;
+const firstKitchenPriceNumber = parseFloat(firstKitchenPriceString);
+const kitchenTotalPrice = document.getElementById("total-price");
+const firstKitchenTotalPriceString = kitchenTotalPrice.innerText;
+const firstKitchenTotalPriceNumber = parseFloat(firstKitchenTotalPriceString);
+
+
+const firstKitchenSum = firstKitchenPriceNumber + firstKitchenTotalPriceNumber;
+kitchenTotalPrice.innerText = firstKitchenSum;
+
+
 
 
 }
@@ -19,6 +31,20 @@ p.innerText = "K.accessories"
 itemsAll.appendChild(p);
 
 
+const secondKitchenPrice = document.getElementById("second-kitchen");
+const secondKitchenPriceString = secondKitchenPrice.innerText;
+const secondKitchenPriceNumber = parseFloat(secondKitchenPriceString);
+const kitchenTotalPrice = document.getElementById("total-price");
+const secondKitchenTotalPriceString = kitchenTotalPrice.innerText;
+const secondKitchenTotalPriceNumber = parseFloat(secondKitchenTotalPriceString);
+
+
+const secondKitchenSum = secondKitchenPriceNumber + secondKitchenTotalPriceNumber;
+kitchenTotalPrice.innerText = secondKitchenSum;
+
+
+
+
 }
 
 function showingKitchen3(){
@@ -27,6 +53,21 @@ const itemsAll = document.getElementById("items-all-show");
 const p = document.createElement("p");
 p.innerText = "Home cooker"
 itemsAll.appendChild(p);
+
+
+
+
+const thirdKitchenPrice = document.getElementById("third-kitchen");
+const thirdKitchenPriceString = thirdKitchenPrice.innerText;
+const thirdKitchenPriceNumber = parseFloat(thirdKitchenPriceString);
+const kitchenTotalPrice = document.getElementById("total-price");
+const thirdKitchenTotalPriceString = kitchenTotalPrice.innerText;
+const thirdKitchenTotalPriceNumber = parseFloat(thirdKitchenTotalPriceString);
+
+
+const thirdKitchenSum = thirdKitchenPriceNumber + thirdKitchenTotalPriceNumber;
+kitchenTotalPrice.innerText = thirdKitchenSum;
+
 
 
 }
@@ -39,6 +80,20 @@ p.innerText = "Sports black cap"
 itemsAll.appendChild(p);
 
 
+
+const firstSportsPrice = document.getElementById("first-sports");
+const firstSportsPriceString = firstSportsPrice.innerText;
+const firstSportsPriceNumber = parseFloat(firstSportsPriceString);
+const sportsTotalPrice = document.getElementById("total-price");
+const firstSportsTotalPriceString = sportsTotalPrice.innerText;
+const firstSportsTotalPriceNumber = parseFloat(firstSportsTotalPriceString);
+
+
+const firstSportsSum = firstSportsPriceNumber + firstSportsTotalPriceNumber;
+sportsTotalPrice.innerText = firstSportsSum;
+
+
+
 }
 
 function showingSportsWear2(){
@@ -47,6 +102,19 @@ const itemsAll = document.getElementById("items-all-show");
 const p = document.createElement("p");
 p.innerText = "Full jersey set"
 itemsAll.appendChild(p);
+
+
+
+const secondSportsPrice = document.getElementById("second-sports");
+const secondSportsPriceString = secondSportsPrice.innerText;
+const secondSportsPriceNumber = parseFloat(secondSportsPriceString);
+const sportsTotalPrice = document.getElementById("total-price");
+const secondSportsTotalPriceString = sportsTotalPrice.innerText;
+const secondSportsTotalPriceNumber = parseFloat(secondSportsTotalPriceString);
+
+
+const secondSportsSum = secondSportsPriceNumber + secondSportsTotalPriceNumber;
+sportsTotalPrice.innerText = secondSportsSum;
 
 
 }
@@ -59,6 +127,21 @@ p.innerText = "Sports cates"
 itemsAll.appendChild(p);
 
 
+
+
+
+const thirdSportsPrice = document.getElementById("third-sports");
+const thirdSportsPriceString = thirdSportsPrice.innerText;
+const thirdSportsPriceNumber = parseFloat(thirdSportsPriceString);
+const sportsTotalPrice = document.getElementById("total-price");
+const thirdSportsTotalPriceString = sportsTotalPrice.innerText;
+const thirdSportsTotalPriceNumber = parseFloat(thirdSportsTotalPriceString);
+
+
+const thirdSportsSum = thirdSportsPriceNumber + thirdSportsTotalPriceNumber;
+sportsTotalPrice.innerText = thirdSportsSum;
+
+
 }
 
 function showingFurniture1(){
@@ -67,6 +150,19 @@ const itemsAll = document.getElementById("items-all-show");
 const p = document.createElement("p");
 p.innerText = "Single relax chair"
 itemsAll.appendChild(p);
+
+
+const firstFurniturePrice = document.getElementById("first-furniture");
+const firstFurniturePriceString = firstFurniturePrice.innerText;
+const firstFurniturePriceNumber = parseFloat(firstFurniturePriceString);
+const furnitureTotalPrice = document.getElementById("total-price");
+const firstFurnitureTotalPriceString = furnitureTotalPrice.innerText;
+const firstFurnitureTotalPriceNumber = parseFloat(firstFurnitureTotalPriceString);
+
+
+const firstFurnitureSum = firstFurniturePriceNumber + firstFurnitureTotalPriceNumber;
+furnitureTotalPrice.innerText = firstFurnitureSum;
+
 
 
 }
@@ -79,6 +175,21 @@ p.innerText = "Children play"
 itemsAll.appendChild(p);
 
 
+
+const secondFurniturePrice = document.getElementById("second-furniture");
+const secondFurniturePriceString = secondFurniturePrice.innerText;
+const secondFurniturePriceNumber = parseFloat(secondFurniturePriceString);
+const furnitureTotalPrice = document.getElementById("total-price");
+const secondFurnitureTotalPriceString = furnitureTotalPrice.innerText;
+const secondFurnitureTotalPriceNumber = parseFloat(secondFurnitureTotalPriceString);
+
+
+const secondFurnitureSum = secondFurniturePriceNumber + secondFurnitureTotalPriceNumber;
+furnitureTotalPrice.innerText = secondFurnitureSum;
+
+
+
+
 }
 
 function showingFurniture3(){
@@ -87,6 +198,21 @@ const itemsAll = document.getElementById("items-all-show");
 const p = document.createElement("p");
 p.innerText = "Flexible sofa"
 itemsAll.appendChild(p);
+
+
+
+const thirdFurniturePrice = document.getElementById("third-furniture");
+const thirdFurniturePriceString = thirdFurniturePrice.innerText;
+const thirdFurniturePriceNumber = parseFloat(thirdFurniturePriceString);
+const furnitureTotalPrice = document.getElementById("total-price");
+const thirdFurnitureTotalPriceString = furnitureTotalPrice.innerText;
+const thirdFurnitureTotalPriceNumber = parseFloat(thirdFurnitureTotalPriceString);
+
+
+const thirdFurnitureSum = thirdFurniturePriceNumber + thirdFurnitureTotalPriceNumber;
+furnitureTotalPrice.innerText = thirdFurnitureSum;
+
+
 
 
 }
