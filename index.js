66b39627194ee536752bdf13b967de620d0abf9d
unknown <imrojtahmid@gmail.com>
2023-08-21 11:40:40 +0600

    updated

---
+++ index.js
@@ -0,0 +1,92 @@
+function showingKitchen1(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "K.accessories"
+itemsAll.appendChild(p);
+
+
+
+
+}
+
+
+function showingKitchen2(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "K.accessories"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingKitchen3(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Home cooker"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingSportsWear1(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Sports black cap"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingSportsWear2(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Full jersey set"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingSportsWear3(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Sports cates"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingFurniture1(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Single relax chair"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingFurniture2(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Children play"
+itemsAll.appendChild(p);
+
+
+}
+
+function showingFurniture3(){
+
+const itemsAll = document.getElementById("items-all-show");
+const p = document.createElement("p");
+p.innerText = "Flexible sofa"
+itemsAll.appendChild(p);
+
+
+}
